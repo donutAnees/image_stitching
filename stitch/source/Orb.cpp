44395dfb21d3ffcf6d8931d1145ae4a2f165f805
donutAnees <pvmohammedanees2003@gmail.com>
2024-02-04 20:03:48 +0530

    implement fast

--- stitch/source/Orb.cpp
+++ stitch/source/Orb.cpp
@@ -5,8 +5,14 @@ ORB::ORB(Mat &image, std::vector<Keypoint> &keypoints, Mat &descriptor)
 
 bool ORB::isPixelKeypoint(int x, int y, int z)
 {
-    int radius = 3; // Radius of the circle around the pixel
-    
+    unsigned char radius = 3; // Radius of the circle around the pixel
+    // the pixels the circle touches are numbered from 1 to 16
+    // initially consider the pixels 1,5,9,13, if atleast 3 of them are above or below the intesity (+/-) threshold then continue, else return false
+    unsigned char intensityAt1 = ;
+    unsigned char intensityAt5 = ;
+    unsigned char intensityAt9 = ;
+    unsigned char intensityAt13 = ; 
+
 }
 
 void ORB::detectKeypoints()
