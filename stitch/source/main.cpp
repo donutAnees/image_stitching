fa4eeac40cb1b73b7775e02fbf55a38956171855
radhikaks09 <radhu.shoba@gmail.com>
2024-02-04 12:41:12 +0530

    main

---
+++ stitch/source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+    if(argc<3){
+        cerr<<"Usage: "<<argv[0]<<" <argument1> <argument2> [<argument3> <argument4>...]"<<endl;
+        return -1;
+    }
+    for(int i=1;i<argc;i++){
+        cout<<"img"<<i<<":"<<argv[i]<<endl;
+    }
+    return 0;
+}
