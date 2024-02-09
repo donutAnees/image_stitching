ed9ef0e0b02ffc3ca0e51a0aaecedd2da4889672
donutAnees <pvmohammedanees2003@gmail.com>
2024-02-09 10:52:02 +0530

    created source dir

--- opencv/source/stitch.cpp
+++ opencv/source/stitch.cpp
@@ -1,16 +1,7 @@
-#include <opencv2/core/mat.hpp>
-#include <opencv2/features2d.hpp>
-#include <vector>
+#include "stitch.hpp"
 
-class stitch
+void stitch::processImage()
 {
-public:
-    std::vector<cv::Mat> &images;
-    std::vector<cv::Mat> &grayscaledImages;
-    cv::Mat &currentStitchedImage;
-    std::vector<cv::KeyPoint> &currentImageKeypoints;
-    cv::Mat &currentImageDescriptor;
-    void processImage()
     {
         uint8_t imageCount = images.size();
         uint8_t middle = images.size() / 2;
@@ -40,4 +31,4 @@ public:
         {
         }
     }
-};
+}
